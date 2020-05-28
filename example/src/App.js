a3f59c2b5d4410359b74d30438851d7b18505c74
sonnbsas <saskia.sonnberg@tieto.com>
2020-05-29 00:07:34 +0300

    rich textbox

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-import MediumProfile from "react-medium-profile";
+import Editor from "react-medium-profile";
+import './index.css';
+import '../node_modules/react-medium-profile/example/node_modules/react-medium-profile/node_modules/react-quill/dist/quill.snow.css'
 
 export default class App extends Component {  
   render() {
     return (    
-        <MediumProfile />        
+      <Editor placeholder={"Kirjutan"} />       
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,56 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import MediumLogo from "./medium.png";
-import Modal, { closeStyle } from "simple-react-modal";
-import FadeIn from "react-fade-in";
+import React from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
-export default class MediumProfile extends Component {
+export default class Editor extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        text: "",
+      }
+      this.handleChange = this.handleChange.bind(this)     
+    };
 
-  render() {
-    return (
-      <div><p>Saskia</p></div>
+      
+    modules = {
+      toolbar: [
         
-    )
-        }
-}
+        ['bold', 'italic', 'underline','strike',
+        {'list': 'ordered'}, {'list': 'bullet'}, 
+        'link', 'image', {'indent': '-1'}, {'indent': '+1'},
+        'clean',
+        { 'color': [] }, { 'script': 'super' }],
+      ],
+    }
+  
+    formats = [
+      
+      'bold', 'italic', 'underline', 'strike', 
+      'list', 'bullet', 
+      'link', 'image','indent', 'color', 'script'
+    ]
+
+    handleChange (value) {
+        this.setState({ text: value });
+        console.log(this.state.text)
+    }
+   
+    render() {
+      return (
+          <div>
+        <div className="text-editor">
+          <ReactQuill theme="snow"
+                      modules={this.modules}
+                      placeholder={this.props.placeholder}
+                      formats={this.formats}
+                      onChange={this.handleChange}
+                      value={this.state.text}>
+          </ReactQuill>
+        </div>
+        <div dangerouslySetInnerHTML={{__html: this.state.text}} className={'notes form-control'} disabled>
+        </div>
+       
+        </div>
+      );
+    }
+  }
